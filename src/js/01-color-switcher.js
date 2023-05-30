9be55ce362a2c34b22540e62e2b42e2ddc9e2713
Iluha3443 <tkacenkoila01@gmail.com>
2023-05-30 20:26:13 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
 
+let intervalId;
+let backgroundColor;
+
+btnStart.addEventListener('click', function() {
+    intervalId = setInterval(function () {
+       btnStart.disabled = true;
+       document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+     
+});
+
+btnStop.addEventListener('click', onColorStop);
+
+function onColorStop() {
+    clearInterval(intervalId);
+    document.body.style.backgroundColor = backgroundColor;
+    btnStart.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
